9b32d462f009688fd82446c8d863c667dde76676
Brandon Braun <brandonbraun653@gmail.com>
2022-06-12 19:21:10 -0700

    Finally got a sim going

--- model/FOCControlScript.m
+++ model/FOCControlScript.m
@@ -1,19 +1,25 @@
 % Set constants for the FOC simulation
-Ls = 0.00022;   % Stator inductance
-Rs = 0.013;     % Stator Resistance
-Psi = 0.03;     % Permanent magnet flux
-gamma = 50000;  % Observer gain
-P = 6;          % Number of poles
-Vdc = 12;       % DC Link voltage
+Ls = 0.00022;       % Stator inductance
+Rs = 0.013;         % Stator Resistance
+Psi = 0.03;          % Permanent magnet flux
+gamma = 50000;      % Observer gain
+P = 6;              % Number of poles
+Vdc = 12;           % DC Link voltage
+W_max_rad = 3140;   % ~30k rpm
 
 % PID Speed Controller
 Kp_speed = 0.459;
 Ki_speed = 6.0;
 Kd_speed = 0.0;
 
+% PID Current Controller
+Kp_i = 0.459;
+Ki_i = 6.0;
+
 % Sample rates 
-Ts = 50e-6;
-Ts_motor = 25e-6;
-Ts_inverter = 25e-6;
-Ts_speed = 1e-3;
+Ts          = 50e-6;
+Ts_motor    = 10e-6;
+Ts_inverter = 10e-6;
+Ts_simulink = 10e-6;
+Ts_speed    = 1e-3;
 
