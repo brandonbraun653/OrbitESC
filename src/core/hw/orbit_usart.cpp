ae2fd038bf6874b968c750f3261a2da71556fa85
Brandon Braun <brandonbraun653@protonmail.com>
2023-01-19 12:56:37 -0700

    Reduce project memory usage

--- src/core/hw/orbit_usart.cpp
+++ src/core/hw/orbit_usart.cpp
@@ -5,7 +5,7 @@
  *  Description:
  *    Orbit USART bus driver
  *
- *  2022 | Brandon Braun | brandonbraun653@protonmail.com
+ *  2022-2023 | Brandon Braun | brandonbraun653@protonmail.com
  *****************************************************************************/
 
 /*-----------------------------------------------------------------------------
@@ -85,21 +85,21 @@ namespace Orbit::USART
     /*-------------------------------------------------------------------------
     Start the logging framework
     -------------------------------------------------------------------------*/
-    // Aurora::Logging::initialize();
-    // Aurora::Logging::setGlobalLogLevel( Aurora::Logging::Level::LVL_TRACE );
+    Aurora::Logging::initialize();
+    Aurora::Logging::setGlobalLogLevel( Aurora::Logging::Level::LVL_TRACE );
 
-    // s_serial_sink.assignChannel( IO::USART::serialChannel );
-    // s_serial_sink.logLevel = Aurora::Logging::Level::LVL_TRACE;
-    // s_serial_sink.enabled  = true;
-    // s_serial_sink.name     = "SerialLog";
+    s_serial_sink.assignChannel( IO::USART::serialChannel );
+    s_serial_sink.logLevel = Aurora::Logging::Level::LVL_TRACE;
+    s_serial_sink.enabled  = true;
+    s_serial_sink.name     = "SerialLog";
 
-    // if ( !s_serial_handle )
-    // {
-    //   s_serial_handle = Aurora::Logging::SinkHandle_rPtr( &s_serial_sink );
-    //   registerSink( s_serial_handle );
-    // }
+    if ( !s_serial_handle )
+    {
+      s_serial_handle = Aurora::Logging::SinkHandle_rPtr( &s_serial_sink );
+      registerSink( s_serial_handle );
+    }
 
-    // RT_HARD_ASSERT( Aurora::Logging::Result::RESULT_SUCCESS == Aurora::Logging::setRootSink( s_serial_handle ) );
+    RT_HARD_ASSERT( Aurora::Logging::Result::RESULT_SUCCESS == Aurora::Logging::setRootSink( s_serial_handle ) );
   }
 
 }    // namespace Orbit::USART
---
+++ src/integration/Aurora/file_types_prj.hpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+ *  File Name:
+ *    file_types_prj.hpp
+ *
+ *  Description:
+ *    Project side configuration of file_types.hpp
+ *
+ *  2023 | Brandon Braun | brandonbraun653@protonmail.com
+ *****************************************************************************/
+
+#pragma once
+#ifndef ORBIT_ESC_AURORA_FILE_TYPES_CONFIGURATION_HPP
+#define ORBIT_ESC_AURORA_FILE_TYPES_CONFIGURATION_HPP
+
+#define AURORA_PRJ_FS_MAX_VOLUMES           ( 1 )
+#define AURORA_PRJ_FS_MAX_OPEN_FILES        ( 2 )
+#define AURORA_PRJ_FS_MAX_FILE_NAME_LEN     ( 16 )
+#define AURORA_PRJ_FS_MAX_DRIVE_PREFIX_LEN  ( 8 )
+
+#endif  /* !ORBIT_ESC_AURORA_FILE_TYPES_CONFIGURATION_HPP */
