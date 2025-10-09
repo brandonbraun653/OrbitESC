9c341ffe2b257f6e7c58eb943f9c6acac485f82c
Brandon Braun <brandonbraun653@protonmail.com>
2025-10-09 14:15:29 -0700

    Integrate trace module with compilation

--- src/core/bootup.cpp
+++ src/core/bootup.cpp
@@ -36,6 +36,7 @@ Includes
 #include <src/core/tasks.hpp>
 #include <src/monitor/orbit_metrics.hpp>
 #include <src/monitor/orbit_monitors.hpp>
+#include <src/trace/trace_examples.hpp>
 
 #if defined( EMBEDDED )
 #include <Thor/lld/common/cortex-m4/system_time.hpp>
@@ -111,6 +112,11 @@ namespace Orbit::Boot
     -------------------------------------------------------------------------*/
     Log::initialize();
     Log::enable();
+
+    /*-------------------------------------------------------------------------
+    Initialize the trace system for data logging and debugging
+    -------------------------------------------------------------------------*/
+    Orbit::Trace::initializeTraceSystem();
 
     /*-------------------------------------------------------------------------
     Power up the peripherals with re-configurable settings
