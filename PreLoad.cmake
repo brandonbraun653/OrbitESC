cbe162a2c5083aba3e920f158fcc06cd71cb3270
Brandon Braun <brandonbraun653@protonmail.com>
2025-10-20 06:15:18 -0700

    Integrate CMake tooling updates

--- PreLoad.cmake
+++
@@ -1,8 +0,0 @@
-# ====================================================
-# Override the toolchain used. This must be done here
-# else CMake won't allow the override.
-# ====================================================
-set(PROJECT_ROOT "${CMAKE_CURRENT_SOURCE_DIR}" CACHE STRING "")
-set(COMMON_TOOL_ROOT "${CMAKE_CURRENT_SOURCE_DIR}/lib/CommonTools" CACHE STRING "")
-
-include("${COMMON_TOOL_ROOT}/cmake/options/toolchain.cmake")
